fa7d67c616ae7eb346224ef3d87de1e7827b456b
tkiilian21 <thiago.kulian@etec.sp.gov.br>
2025-04-25 10:28:19 -0300

    documentando codigo

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ fetch(`https://dattebayo-api.onrender.com/${route}`,{
     })
     .then((data) => {
         console.log(data.characters[0].images[0]) // esta buscando dentro do array caracters a posicao da primeira informação.
-        console.log(data.characters[0].jutsu)     
+        console.log(data.characters[0].jutsu)  // busca a primeira posição do array caracters, e a lista de array jutsu   
 
     })
 
